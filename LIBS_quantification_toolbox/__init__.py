a0e4caa3662637f8a15e0e2a4e0f9b14bc0b57d8
Nachotron18 <99397078+NachoVA18@users.noreply.github.com>
2025-04-30 10:28:11 +0200

    Comentario en init

--- LIBS_quantification_toolbox/__init__.py
+++ LIBS_quantification_toolbox/__init__.py
@@ -1,3 +1,10 @@
+'''
+This is the __init__.py file for the LIBS quantification toolbox package.
+To use it you will need to run this lines::
+import sys
+sys.path.append('../LIBS_QUANTIFICATION_INTA')
+from LIBS_quantification_toolbox import plot_spectra
+'''
 from .preprocesado import *
 from .visualizacion import *
 from .lectura import *
